d482c95d520b4681a0f8a7be36b0caf96abfb1b9
DamirPavlik <damir.pavlik@gmail.com>
2024-09-18 10:12:25 +0200

    feat: add router to main.go

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("hello world")
+
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("Port is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
