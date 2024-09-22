4e0a66b2de988c032c1c538a3cca7ceaa7671696
DamirPavlik <damir.pavlik@gmail.com>
2024-09-22 23:23:51 +0200

    feat: add feeds post route

--- main.go
+++ main.go
@@ -55,6 +55,9 @@ func main() {
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.middlwareAuth(apiCfg.handlerGetUser))
+
+	v1Router.Post("/feeds", apiCfg.middlwareAuth(apiCfg.handlerCreateFeed))
 
 	router.Mount("/v1", v1Router)
 
